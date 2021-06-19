28ae933f8d37055c1a74e4fa1b46e0da3f5fe510
haruto <hinoue@skylight.co.jp>
2021-06-19 22:45:49 +0900

    エラー修正版

--- routes/te_newrecord.js
+++ routes/te_newrecord.js
@@ -84,40 +84,8 @@ router.get('/', async function(req, res, next) {
     // subtotal:subtotal,
     // job_no:job_no,
      }
+     res.render('te_newrecord', opt);
     });
-
-//ページが読み込まれた際の初期画面
-router.get('/',function(req,res,next){
-
-    let opt = {
-        title: '（交通費）新規登録ページ',
-        message: '各項目を入力してください',
-        price: 'placeholder="自動計算（ICカード利用時料金）"',
-        moveDate:'placeholder="移動した日付・時刻が自動で追加されます"',
-        year: '',
-        month:'',
-        day:'',
-        sStart: '',
-        sWaypoint: '',
-        sGoal: '',
-        title: '経費新規登録ページ',
-        message: '各項目を入力してください',
-        amount: 'placeholder="自動計算（ICカード利用時料金）"',
-        moveDate:'',
-        moveDate2: "移動した日付・時刻が自動で追加されます",
-        year:'',
-        month:'',
-        day:'',
-        trans_from: '',
-        trans_waypoint: '',
-        trans_to: '',
-        id:'',
-        flg:''
-    };
-    res.render('te_newrecord', opt);
-  })
-  
-
 
 
 
